dbc4b30b4c463ee99b4d2ff48f3b2fac6a27c3a0
Ward Coessens <wcoessen@thoughtworks.com>
2020-02-28 16:01:31 +0100

    ec #5 Upgrade dependencies to be compatible with JDK 9 and up

--- front-end/project.clj
+++ front-end/project.clj
@@ -1,11 +1,11 @@
 (defproject front-end "0.1.0-SNAPSHOT"
   :description "Front-end webapp for assessment"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.6"]
                  [com.thoughtworks/common-utils "0.1.0-SNAPSHOT"]
-                 [compojure "1.5.0"]
+                 [compojure "1.6.1"]
                  [hiccup "1.0.5"]
-                 [http-kit "2.1.18"]
+                 [http-kit "2.4.0-alpha6"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.7"]
                  [ring/ring-devel "1.4.0"]]
@@ -15,8 +15,8 @@
   :target-path "target/%s"
   :resources-paths ["resources/"]
   :profiles {:uberjar {:aot :all}
-             :dev     {:dependencies [[peridot "0.4.3"]
-                                      [midje "1.8.3"]]
+             :dev     {:dependencies [[peridot "0.5.2"]
+                                      [midje "1.9.9"]]
                        :plugins      [[lein-midje "3.2"]]
                        :aliases      {"test" ["midje"]}}
              })
